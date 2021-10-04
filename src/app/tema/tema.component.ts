6451f2f425556a2733fe4b38087005f5e6dce39a
Paulopdm <paulo.mfn@gmail.com>
2021-10-04 00:00:43 -0300

    componente de tema

---
+++ src/app/tema/tema.component.ts
@@ -0,0 +1,25 @@
+import { environment } from 'src/environments/environment.prod';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    if(environment.token == ''){
+      alert('Sua sessão expirou, faça o login novamente')
+      this.router.navigate(['/entrar'])
+    }
+  }
+
+}

4abe2d06c5322e8557f21d19ad42472786472202
Paulopdm <paulo.mfn@gmail.com>
2021-10-03 21:27:36 -0300

    implementação das funcionalidades de logout

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -23,5 +24,14 @@ export class AuthService {
 
     return this.http.post<Usuario>('https://blogpessoalpaulopdm.herokuapp.com/usuarios/cadastrar', usuario)
 
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
